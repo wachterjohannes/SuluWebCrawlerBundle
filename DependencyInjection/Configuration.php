9110695602576e86f9577d161ba204a0e82eae2c
Wachter Johannes <johannes.wachter@massiveart.at>
2015-02-06 10:19:12 +0100

    init bundle

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\WebCrawlerBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * {@inheritdoc}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('sulu_web_crawler');
+
+        return $treeBuilder;
+    }
+}
